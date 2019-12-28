58cdb3f0b8d2d217c470fba587c61242eae87185
Dmitriy-cripto <dmitriystadnikov17@gmail.com>
2019-12-28 09:18:45 +0200

    18 Added in build.gradle testGroup

--- src/pages/HomePageHelper.java
+++ src/pages/HomePageHelper.java
@@ -3,10 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 import java.util.List;
 
--- src/tests/WebHomePageTests.java
+++ src/tests/WebHomePageTests.java
@@ -1,16 +1,9 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.WebPageHomeHelper;
-
-import java.util.List;
 
 public class WebHomePageTests extends TestBase{
     WebPageHomeHelper webPageHome;
